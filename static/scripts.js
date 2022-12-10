208130795b9b61a4b0f1af81521891cbb0edfb42
S1im_Sh4dy <mihakurd@gmail.com>
2022-12-10 16:23:40 +0300

    change color 'Home' button

--- static/scripts.js
+++ static/scripts.js
@@ -11,14 +11,15 @@ $(function() {
   function navbarFixed(){
       if ( $('.header_area').length ){
           $(window).scroll(function() {
-              var scroll = $(window).scrollTop();
-              if (scroll >= nav_offset_top ) {
+            let scroll = $(window).scrollTop();
+            if (scroll >= nav_offset_top ) {
                   $(".header_area").addClass("navbar_fixed");
               } else {
                   $(".header_area").removeClass("navbar_fixed");
               }
           });
-      };
-  };
+      }
+  }
   navbarFixed();
 });
+
